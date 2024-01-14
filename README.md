a5e3cb2e510d6a42c6c11538e933e2e6a81a634c
cloudframework <13601746-cloudframework@users.noreply.replit.com>
2024-01-14 07:18:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install backend-core-php8 Framework
+This framework is optimized to work with GCP (Google Cloud Platform) resources like Datastore, SQL, BigQuery etc..
+
+## Install de FRAMEWORK
+```
+composer require cloudframework-io/backend-core-php8
+php vendor/cloudframework-io/backend-core-php8/install.php replit
+```
+Now Deploy your server.
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+if(getenv('CF_GOOGLE_APPLICATION_CREDENTIALS')) {
+  putenv('GOOGLE_APPLICATION_CREDENTIALS=/tmp/credentials.json');
+  if(!is_file('/tmp/credentials.json')) 
+    file_put_contents('/tmp/credentials.json',getenv('CF_GOOGLE_APPLICATION_CREDENTIALS'));
+}
+include "vendor/cloudframework-io/backend-core-php8/src/dispatcher.php";
